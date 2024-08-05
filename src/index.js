323e7ca6ee6dc0b19b3a21e12732ac3cc10cd60e
Mike Novikov <NovikovM18>
2024-08-05 21:40:29 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCK51gyqHkDuUJPXABowAWGCUgT2i4NR2A",
+  authDomain: "dava-school.firebaseapp.com",
+  projectId: "dava-school",
+  storageBucket: "dava-school.appspot.com",
+  messagingSenderId: "533828497865",
+  appId: "1:533828497865:web:79d9fa15581f9a5d5d7818"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +27,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
